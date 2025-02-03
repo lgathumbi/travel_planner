5608cae897883061c545ca240923454160001cd9
lorna gathumbi <nyawiralornah@gmail.com>
2025-02-03 07:15:34 +0300

    added gitignore

--- server/app.py
+++ server/app.py
@@ -7,13 +7,15 @@ from flask_restful import Api
 from model import db, Users, Itinerary, Destination, ItineraryDestination
 from flask_cors import CORS
 from datetime import datetime
+from dotenv import load_dotenv
+load_dotenv()
 
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
